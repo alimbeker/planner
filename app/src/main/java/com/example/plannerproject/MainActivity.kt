bd5e00f5cd97d4fde5f593b6c1345d718060f838
Amir Alimbekerov <200103054@stu.sdu.edu.kz>
2022-10-27 19:56:13 +0600

    find ID

--- app/src/main/java/com/example/plannerproject/MainActivity.kt
+++ app/src/main/java/com/example/plannerproject/MainActivity.kt
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.plannerproject.model.CardData
+import com.example.plannerproject.view.ItemAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var addsBtn:FloatingActionButton
+    private lateinit var recv:RecyclerView
+    private lateinit var cardList:ArrayList<CardData>
+    private lateinit var itemAdapter:ItemAdapter
+
+
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
