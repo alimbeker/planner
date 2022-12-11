316722d2f68cec0fc20ab7788e9a17dea272b858
Galymbek Batyrbek <ashykbaev03@gmail.com>
2022-12-12 03:59:19 +0600

    changable header gmails and action bars and logout

--- app/src/main/java/com/example/plannerproject/SignUpActivity.kt
+++ app/src/main/java/com/example/plannerproject/SignUpActivity.kt
@@ -1,15 +1,28 @@
 package com.example.plannerproject
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.plannerproject.databinding.ActivitySignInBinding
 import com.example.plannerproject.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
-    private lateinit var binding:ActivitySignUpBinding
+    //ViewBinding
+    private lateinit var binding: ActivitySignUpBinding
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+    //ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+    //FirebaseAuth
     private lateinit var firebaseAuth: FirebaseAuth
+    private var email=""
+    private var password=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +30,30 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Configure Actionbar //enable back button
+        actionBar=supportActionBar!!
+        actionBar.title="Login"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowCustomEnabled(true)
+
+        //configure progress dialog
+        progressDialog=ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Creating account In...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init FirebaseAuth
         firebaseAuth = FirebaseAuth.getInstance()
 
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+
+        //handle click, begin signup
         binding.button.setOnClickListener{
+            //validate data
+            validateData()
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
@@ -46,5 +76,57 @@ class SignUpActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun validateData() {
+        //get data
+        email=binding.emailEt.text.toString().trim()
+        password=binding.passET.text.toString().trim()
+
+        //validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailEt.error = "Invalid email format"
+        }
+        else if(TextUtils.isEmpty(password)){
+            //no password entered
+            binding.passET.error = "Please enter password"
+        }
+        else if(password.length<6){
+            //password length less than 6
+            binding.passET.error = "Password must atleast 6 characters long"
+        }
+        else{
+            //data is valid,continue sign up
+            firebaseSignUp()
+        }
+    }
+
+    private fun firebaseSignUp() {
+        //show progress
+        progressDialog.show()
+        //create account
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //signup success
+                progressDialog.dismiss()
+                //get current user
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this,"Account created with email $email", Toast.LENGTH_SHORT).show()
+
+                //open Home fragment
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e->
+                //signup failure
+                progressDialog.dismiss()
+                Toast.makeText(this,"SignUp failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed() //go back to previous activity,when back button of actionbar clicked
+        return super.onSupportNavigateUp()
     }
 }
